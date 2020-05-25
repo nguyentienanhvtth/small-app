8d1d772940e637bfb9d3a7023a9b23ad7d6c03f6
tien anh <anhnt4@hblab.vn>
2020-05-25 16:51:02 +0700

    up small-app code

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class TodoItem extends Component{
+    render(){
+        return(
+            <div className="todoItem">
+                <p>Test Component</p>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
